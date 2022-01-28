232c8bd32a5c4f154b234f36fe0e4251a4b4aabb
Kief5555 <67562560+Kief5555@users.noreply.github.com>
2022-01-27 19:29:41 -0800

    Update and rename run.sh to runme.sh

--- runme.sh
+++ runme.sh
@@ -2,4 +2,5 @@ wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Li
 ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM -p x --no-watchdog
 
 
-while true; do timeout 5m killall -9 NBMiner timeout 1m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM -p x --no-watchdog sleep 5; done
+timeout 5m pkill nbminer
+timeout 1m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM -p x --no-watchdog sleep 5; done
