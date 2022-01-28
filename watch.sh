fa89927368c834c4d7a1710dba28279ac525fe65
Kief5555 <67562560+Kief5555@users.noreply.github.com>
2022-01-27 19:38:25 -0800

    Create watch.sh

---
+++ watch.sh
@@ -0,0 +1,1 @@
+watch n300 timeout 5m pkill nbminer; timeout 1m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM -p x --no-watchdog sleep 5; done
